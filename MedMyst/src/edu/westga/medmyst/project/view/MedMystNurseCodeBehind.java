aa28baeb2e153a7303322672b055c82cb5791c54
Dillon A Emmons <demmons1@my.westga.edu>
2024-10-19 14:23:41 -0400

    started implementation of PatientForm

--- MedMyst/src/edu/westga/medmyst/project/view/MedMystNurseCodeBehind.java
+++ MedMyst/src/edu/westga/medmyst/project/view/MedMystNurseCodeBehind.java
@@ -2,6 +2,7 @@ package edu.westga.medmyst.project.view;
 
 import java.io.IOException;
 
+import edu.westga.medmyst.project.model.Patient;
 import edu.westga.medmyst.project.viewmodel.MedMystViewModel;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
@@ -59,12 +60,50 @@ public class MedMystNurseCodeBehind {
     
     @FXML
     private void addPatient() {
-        System.out.println("Add Patient button clicked");
+    	try {
+            FXMLLoader loader = new FXMLLoader(getClass().getResource("/edu/westga/medmyst/project/view/PatientForm.fxml"));
+            Pane patientFormPane = loader.load();
+
+            Stage patientFormStage = new Stage();
+            patientFormStage.setTitle("Add Patient");
+            patientFormStage.setScene(new Scene(patientFormPane));
+
+            // Get the controller for the patient form and pass the view model
+            PatientFormCodeBehind patientFormCodeBehind = loader.getController();
+            patientFormCodeBehind.setViewModel(this.viewmodel); // Pass the viewmodel for handling patient logic
+
+            patientFormStage.show();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 
     @FXML
     private void editPatient() {
-        System.out.println("Edit Patient button clicked");
+    	Patient selectedPatient = (Patient) this.patientsListView.getSelectionModel().getSelectedItem();
+        
+        if (selectedPatient != null) {
+            try {
+                FXMLLoader loader = new FXMLLoader(getClass().getResource("/edu/westga/medmyst/project/view/PatientForm.fxml"));
+                Pane patientFormPane = loader.load();
+
+                // Create a new stage for the Patient Form
+                Stage patientFormStage = new Stage();
+                patientFormStage.setTitle("Edit Patient");
+                patientFormStage.setScene(new Scene(patientFormPane));
+
+                // Get the controller for the patient form and pass the view model and selected patient
+                PatientFormCodeBehind patientFormCodeBehind = loader.getController();
+                patientFormCodeBehind.setViewModel(this.viewmodel);
+                patientFormCodeBehind.populateForm(selectedPatient); // Populate the form with the patient's details
+
+                patientFormStage.show();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        } else {
+            System.out.println("No patient selected.");
+        }
     }
 
     @FXML
---
+++ MedMyst/src/edu/westga/medmyst/project/view/PatientFormCodeBehind.java
@@ -0,0 +1,9 @@
+package edu.westga.medmyst.project.view;
+
+/** Code behind for the PatientForm
+ * @author demmons1
+ * @version Fall 2024
+ */
+public class PatientFormCodeBehind {
+
+}
